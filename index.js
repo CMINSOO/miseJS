f1cdd922e54a9af4263a825f221b4f4425c7e09e
chominsoo <soochomin@gmail.com>
2025-02-13 22:20:49 +0900

    feat/index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql2/promise");
+
+async function insertProblem() {
+  const connection = await mysql.createConnection({
+    host: "backendtest.c7gaw6asmxbo.ap-northeast-2.rds.amazonaws.com",
+    user: "admin",
+    password: "peter0524!010",
+    database: "backendTest",
+  });
+
+  //   const sample = ["돌아가나?"];
+  const description = [
+    "Given an array of integers, return indices of the two numbers such that they add up to a specific target.",
+    "Reverse a singly linked list.",
+    "Determine if a 9x9 Sudoku board is valid.",
+    "Find the longest common prefix string amongst an array of strings.",
+    "Merge two sorted linked lists and return it as a new sorted list.",
+    "Given an array of integers, find the maximum product of three numbers.",
+    "Find the minimum path sum from top left to bottom right of a matrix.",
+    "Implement a queue using stacks.",
+    "Check if a string is a valid palindrome.",
+    "Convert a binary tree to a doubly linked list in place.",
+  ];
+
+  const insertValue = description.map(async (e) => {
+    return await connection.query(
+      "INSERT INTO temp_DevProblemMeta (description) VALUES (?)",
+      [e]
+    );
+  });
+  const result = await Promise.all(insertValue);
+
+  const devId = result.map((e) => e[0].insertId);
+  const prodValue = devId.map((e) => [e]);
+  console.log("#", prodValue);
+
+  await connection.query(
+    "INSERT INTO temp_ProdProblemMeta (devproblemmeta_id) VALUES ?",
+    [prodValue]
+  );
+  await connection.end();
+}
+insertProblem();
